37b33df4703ebd0e8dc021bab93d293ec6a89e71
Atakan Akkan <atakan-akkan@hotmail.com>
2023-08-20 16:39:23 +0300

    app.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,14 @@ import Header from "./components/Header/Header";
 import SwitchTab from "./components/Hero/SwitchTab";
 import Products from "./components/Product-Area/Products";
 import ProductInfo from "./components/Product-Area/ProductInfo";
-import Deneme from "./Deneme";
 function App() {
+
   return (
     <>
       <Header />
       <Routes>
         <Route
-<<<<<<< HEAD
           path=":homepage"
-=======
-          path="/"
->>>>>>> 47a1a4f1dd3e921bbb18ff138f5b618f0730b613
           element={
             <>
               <SwitchTab />
@@ -28,8 +24,7 @@ function App() {
           />
           <Route path=":homepage/:durum/:id" element={<ProductInfo />} />
       </Routes>
-
-      {/* <Deneme /> */}
+     
     </>
   );
 }
